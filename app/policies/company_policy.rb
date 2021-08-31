479076352057b1505cdee2d98f32a785be1a0802
David <davidhazagury@gmail.com>
2021-08-31 09:14:34 +0200

    Companies can only be CUD, by site admin

---
+++ app/policies/company_policy.rb
@@ -0,0 +1,27 @@
+class CompanyPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+
+  def new?
+    user.role? :admin
+  end
+  def create?
+    user.role? :admin
+  end
+
+  def edit?
+    user.role? :admin
+  end
+
+  def update?
+    user.role? :admin
+  end
+
+  def destroy?
+    user.role? :admin
+  end
+
+end
