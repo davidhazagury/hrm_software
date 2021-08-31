3631f88f8f3675653980eabf258c16d4aa9b17cc
David <davidhazagury@gmail.com>
2021-08-31 08:12:34 +0200

    Added comments and trackable user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  #We have turn on the timeout => which automatically logs the user out after 2 hours(set it by us), and the trackable
+  #module to being able to track users.
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :timeout_in
+         :recoverable, :rememberable, :validatable, :timeout_in, :trackable
 end
