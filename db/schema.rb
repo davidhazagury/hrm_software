7b539bacd3d6293cdf1eb97ecb6d807f908ee19f
David <davidhazagury@gmail.com>
2021-08-31 09:10:55 +0200

    Created Company model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_31_055430) do
+ActiveRecord::Schema.define(version: 2021_08_31_061725) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "assignments", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "role_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["role_id"], name: "index_assignments_on_role_id"
+    t.index ["user_id"], name: "index_assignments_on_user_id"
+  end
 
   create_table "roles", force: :cascade do |t|
     t.string "role_name"
@@ -40,4 +49,6 @@ ActiveRecord::Schema.define(version: 2021_08_31_055430) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "assignments", "roles"
+  add_foreign_key "assignments", "users"
 end
