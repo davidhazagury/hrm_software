b9962be85de3ccfdb7213ab1e9e797603ac18089
David <davidhazagury@gmail.com>
2021-10-17 17:41:56 +0200

    Added test to sick class

--- app/models/sick.rb
+++ app/models/sick.rb
@@ -1,9 +1,9 @@
 class Sick < ApplicationRecord
-  #validates :start_date_sickness, presence: true
-  #validates :rejoin_date_sickness, presence: true
-  #validates :comment, presence: true, length: { minimum: 15, maximum: 30 }
+  validates :start_date_sickness, presence: true
+  validates :rejoin_date_sickness, presence: true
+  validates :comment, presence: true, length: { minimum: 15, maximum: 30 }
   # Rejoin date must be greater than the start date
-  #validate :check_dates
+  validate :check_dates
   belongs_to :worker
 
   private
--- spec/models/sick_spec.rb
+++ spec/models/sick_spec.rb
@@ -2,9 +2,23 @@ require 'rails_helper'
 
 RSpec.describe Sick, type: :model do
   context 'Validations for Sick Leaves' do
-    it 'ensures workers id presence' do
+    it 'ensures start date of sick leave' do
       # We want .save to return false => This is means the instance hasn't being saved
       sick = Sick.new().save
+      expect(sick).to eq(false)
+    end
+    it 'ensures rejoin date of sick leave' do
+      # We want .save to return false => This is means the instance hasn't being saved
+      sick = Sick.new(start_date_sickness:'2020-01-20').save
+      expect(sick).to eq(false)
+    end
+    it 'ensures sick has a comment' do
+      sick = Sick.new(start_date_sickness:'2020-01-20', rejoin_date_sickness: '2020-10-01').save
+      expect(sick).to eq(false)
+    end
+    it 'ensures workers id presence' do
+      # We want .save to return false => This is means the instance hasn't being saved
+      sick = Sick.new(start_date_sickness:'2020-01-20', rejoin_date_sickness: '2020-10-01', comment:'Test with minimun letters').save
       # we force sick to not have a worker_id, it's returning red because,
       # our expectation hasn't been fullfield. (It woud've saved the instance)
       expect(sick).to eq(false)
