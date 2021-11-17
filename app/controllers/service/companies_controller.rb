ebd64c3d200bdf3739ecede224d20d6034743b36
David <davidhazagury@gmail.com>
2021-11-17 20:41:01 +0100

    Added bucket creation

--- app/controllers/service/companies_controller.rb
+++ app/controllers/service/companies_controller.rb
@@ -10,6 +10,12 @@ class Service::CompaniesController < ApplicationController
     @company = Company.new(company_params)
     if @company.save
       # SEND EMAIL CONFIRMATION DEVISE CONFIRMATION!
+      # Create a bucket for eac company that sign's up. When do we create it? when the user is confirmed? When the company is created?
+      #c = Aws::S3::Client.new(region: 'eu-west-1', access_key_id: ENV['AWS_ACCESS_KEY_ID'], secret_access_key: ENV['SECRET_ACCESS_KEY'])
+      # bucket_name = "#{@company.name_of_company.parameterize}-#{@company.id}"
+      #c.create_bucket(bucket: bucket_name)
+      # MAKE THE BUCKET BLOCK ALL PUBLIC!
+      # USE CREATE BUCKET METHOD IN THIS WEB: https://docs.aws.amazon.com/sdk-for-ruby/v3/developer-guide/s3-example-create-buckets.html
     else
       render :new
     end
